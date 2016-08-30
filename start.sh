e99823279949ab767947aa9f64d9645e8c8c1fbb
Alan Hortz <alan.hortz@gmail.com>
2016-08-30 22:18:06 +0200

    simplify Dockerfile

--- start.sh
+++ start.sh
@@ -5,17 +5,6 @@ echo "Set up Bluetooth"
 mkdir -p /mnt/root
 mount --bind / /mnt/root
 
-# firmware_location=/lib/firmware/ti-connectivity
-# mount_point=/mnt/root
-
-# if [[ ! -e $mount_point ]]; then
-#     mkdir -p $mount_point
-#     mount --bind / $mount_point
-# elif [[ ! -d $mount_point ]]; then
-#     echo "$mount_point already exists but is not a directory" 1>&2
-# fi
-
-# cp $mount_point$firmware_location'/*' $firmware_location'/'
 cp /mnt/root/lib/firmware/ti-connectivity/* /lib/firmware/ti-connectivity/
 
 
