5bd94be97d5fcfd777734952f3e56c8859d38c51
Alan Hortz <alan.hortz@gmail.com>
2016-08-30 15:05:27 +0200

    Access the shadowed firmware files and load them
    The directory /lib/firmware/ti-connectivity is mounted from the HostOS and files copied by the apt-get install bb-wl18xx-firmware are still there but shadowed by the mount.
    
    A possible workaround comes from http://superuser.com/questions/389255/is-it-possible-to-access-files-shadowed-by-a-mount. Mount at runtime (here the startup shell script) the /lib/firmware/ti-connectivity and copy back the files into the original directory.

--- start.sh
+++ start.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 echo "Set up Bluetooth"
+
+firmware_location=/lib/firmware/ti-connectivity
+mount_point=/mnt/root
+
+if [[ ! -e $mount_point ]]; then
+    mkdir $mount_point
+    mount --bind $firmware_location $mount_point$firmware_location
+elif [[ ! -d $mount_point ]]; then
+    echo "$mount_point already exists but is not a directory" 1>&2
+fi
+
+cp $mount_point$firmware_location'/*' $firmware_location
 # Seem to have to run in twice, as the first run times out at the moment
 bb-wl18xx-bluetooth
 bb-wl18xx-bluetooth
@@ -11,4 +23,7 @@ while : ; do
   echo "Scan for Bluetooth devices"
   hcitool scan
   sleep 15
+  echo "Scan for Bluetooth LE devices"
+  hcitool scanle
+  sleep 15  
 done
